50a619842b9a9ed17d62e101e1846d3023e4cebc
Avii-KS <avi.k.s.4653@gmail.com>
2025-02-22 09:42:09 +0530

    Initial commit

---
+++ src/components/button.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Button = ({ onClick, text }) => {
+  return (
+    <button
+      className="px-4 py-2 bg-white-500 text-white rounded-md hover:bg-red-600 transition"
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+};
+export default Button;
